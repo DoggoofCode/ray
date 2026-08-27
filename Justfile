0ee5d947472d335eaaa104ab2da7f3f43227ee13
Ved Jaggi <ved@vedjaggi.com>
2026-08-27 10:39:52 +0800

    spinning gausian balls

--- Justfile
+++ Justfile
@@ -1,7 +1,7 @@
 
 render:
     cargo run --release
-    ffmpeg -framerate 30 -i frames/frame%04d.ppm -c:v libx264 -pix_fmt yuv420p output.mp4
+    ffmpeg -framerate 24 -i frames/frame%04d.ppm -c:v libx264 -pix_fmt yuv420p output.mp4
 
 clean:
     cargo clean
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod rays;
 
+use std::f32::consts::{PI, TAU};
+
 use glam::{Mat3, Vec3};
+use libm::{cosf, fabsf, sinf};
 
 use crate::common::{Angle, Camera, Node, Splat};
 
@@ -10,43 +13,47 @@ fn main() {
     let mut cam = Camera::new(
         400,
         400,
-        0.7,
-        Node::new(Vec3::new(0., 0., 2.), Angle::new(0.0, 0.0)),
+        0.5,
+        Node::new(Vec3::new(0., 0., 0.), Angle::new(0.0, 0.0)),
     );
 
-    let mut splats = vec![
-        Splat {
-            position: Vec3::new(10., 0., 0.),
-            scale: Mat3::IDENTITY,
-            color: |_c| Vec3::new(1., 0., 0.),
-        },
+    let splats = vec![
         Splat {
             position: Vec3::new(15., 0., 0.),
             scale: Mat3::IDENTITY,
-            color: |_c| Vec3::new(0., 1., 0.),
+            color: |c: Angle| -> Vec3 {
+                let mod_theta = fabsf(c.theta) % TAU;
+                Vec3::new(0., 1. - mod_theta / TAU, mod_theta / TAU)
+            },
+        },
+        Splat {
+            position: Vec3::new(10., 0., 0.),
+            scale: Mat3::IDENTITY,
+            color: |c: Angle| -> Vec3 {
+                let mod_theta = fabsf(c.theta) % TAU;
+                Vec3::new(1. - mod_theta / TAU, mod_theta / TAU, 0.)
+            },
         },
     ];
 
-    // let splats2 = vec![
-    //     Splat {
-    //         position: Vec3::new(10., 0., 0.),
-    //         scale: Mat3::IDENTITY,
-    //         color: |_c| Vec3::new(0., 1., 0.),
-    //     },
-    //     Splat {
-    //         position: Vec3::new(15., 0., 0.),
-    //         scale: Mat3::IDENTITY,
-    //         color: |_c| Vec3::new(1., 0., 0.),
-    //     },
-    // ];
-
-    for index in 0..8 {
+    let mut percentage: f32;
+    let frames = 48;
+    for index in 0..frames {
+        percentage = index as f32 / frames as f32;
+        cam.node.angle = Angle::new(percentage * TAU, -0.56);
+        cam.node.position = Vec3::new(
+            12.5 + 12.5 * cosf(TAU * percentage + PI),
+            12.5 * sinf(TAU * percentage + PI),
+            8.,
+        );
+        println!(
+            "Camera Position: {:?}: {:?}",
+            cam.node.position, cam.node.angle
+        );
         println!("Starting with {index}");
-        // cam.node.position += Vec3::Y * (10. / 24.);
         cam.render(&splats);
         cam.write(&index).unwrap();
         println!("Completed with {index}");
-        splats[0].position += 0.5 * Vec3::Z;
     }
 
     // println!("{cam:?}");
