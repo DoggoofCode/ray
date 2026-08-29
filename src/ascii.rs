0efc2878ed35d3b93bd61b854b49bde53c67a77f
Ved Jaggi <ved@vedjaggi.com>
2026-08-29 17:04:36 +0800

    background ascii rendering

---
+++ src/ascii.rs
@@ -0,0 +1,138 @@
+use std::io::{self, Write};
+
+use crossterm::{
+    cursor::{Hide, MoveTo, Show},
+    event::{self, Event, KeyCode},
+    execute,
+    terminal::{self, Clear, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+use crate::common::{Camera, ScreenRGB};
+
+fn push_u8(buf: &mut Vec<u8>, n: u8) {
+    if n >= 100 {
+        buf.push(b'0' + n / 100);
+        buf.push(b'0' + (n / 10) % 10);
+        buf.push(b'0' + n % 10);
+    } else if n >= 10 {
+        buf.push(b'0' + n / 10);
+        buf.push(b'0' + n % 10);
+    } else {
+        buf.push(b'0' + n);
+    }
+}
+
+fn push_amp(buf: &mut Vec<u8>, rgb: (u8, u8, u8)) {
+    buf.extend_from_slice(b"\x1b[48;2;");
+    push_u8(buf, rgb.0);
+    buf.push(b';');
+    push_u8(buf, rgb.1);
+    buf.push(b';');
+    push_u8(buf, rgb.2);
+    buf.push(b'm');
+    buf.push(b' ');
+}
+
+pub fn render(virtual_screen: &Camera) -> std::io::Result<()> {
+    let (width, height) = crossterm::terminal::size()?;
+    let virtual_aspect_ratio = virtual_screen.width as f32 / virtual_screen.height as f32;
+    let aspect_ratio = width as f32 / height as f32;
+
+    let scale_ratio: f32;
+    let (real_image_width, real_image_height): (f32, f32);
+    if aspect_ratio < virtual_aspect_ratio {
+        // Use width
+        scale_ratio = virtual_screen.width as f32 / width as f32; // Number of virtual pixels per real pixel
+        println!("{scale_ratio}");
+        (real_image_width, real_image_height) =
+            (width as f32, virtual_screen.height as f32 / scale_ratio);
+    } else {
+        // Use height
+        scale_ratio = virtual_screen.height as f32 / height as f32; // Number of virtual pixels per real pixel
+        (real_image_width, real_image_height) =
+            (virtual_screen.width as f32 / scale_ratio, height as f32);
+        println!("{scale_ratio}");
+    }
+    let kernel_size: u16 = aspect_ratio as u16;
+    println!("Real: {width} {height} 800, 800");
+
+    // let real_image_width = 12;
+    // let real_image_height = 12;
+
+    println!("Real Image {} {}", real_image_height, real_image_width);
+
+    let mut image_buffer: Vec<u8> =
+        Vec::with_capacity(real_image_height as usize * real_image_width as usize * 16);
+    for image_height in 0..real_image_height as u16 {
+        for image_width in 0..real_image_width as u16 {
+            let (mut r, mut g, mut b): (u16, u16, u16) = (0, 0, 0);
+            for kernel_x in 0..kernel_size {
+                for kernel_y in 0..kernel_size {
+                    let color = virtual_screen.screen[virtual_screen.width as usize
+                        * ((image_height * scale_ratio as u16) + kernel_y) as usize
+                        + (image_width * scale_ratio as u16) as usize
+                        + kernel_x as usize];
+                    r += color.r as u16;
+                    g += color.g as u16;
+                    b += color.b as u16;
+                }
+            }
+            let k_squared = kernel_size * kernel_size;
+            r /= k_squared;
+            g /= k_squared;
+            b /= k_squared;
+            push_amp(&mut image_buffer, (r as u8, g as u8, b as u8));
+        }
+        image_buffer.extend_from_slice(b"\x1b[0m");
+        image_buffer.extend_from_slice(b"\n\r");
+    }
+
+    // panic!("asdf");
+
+    let mut stdout = io::stdout();
+
+    // Setup
+    terminal::enable_raw_mode()?;
+    execute!(stdout, EnterAlternateScreen, Hide)?;
+
+    // One buffer for the entire frame
+    let mut buffer: Vec<u8> = Vec::with_capacity(64 * 1024);
+
+    loop {
+        if event::poll(std::time::Duration::from_millis(10))?
+            && let Event::Key(key) = event::read()?
+            && key.code == KeyCode::Esc
+        {
+            break;
+        }
+
+        buffer.clear();
+
+        // Build frame
+        execute!(stdout, MoveTo(0, 0), Clear(terminal::ClearType::All))?;
+        // buffer.extend_from_slice(b"\x1b[2J\x1b[H");
+        buffer.extend_from_slice(&image_buffer);
+
+        // One syscall/write
+        stdout.write_all(&buffer)?;
+        stdout.flush()?;
+
+        // input / timing...
+    }
+
+    // Cleanup
+    execute!(stdout, Show, LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
+}
+
+/* #[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_render() {
+        render().unwrap();
+    }
+} */
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 mod rays;
 
+use std::env;
 use std::f32::consts::{PI, TAU};
 
 use glam::{Mat3, Vec3, mat3};
 use libm::{cosf, fabsf, sinf};
 
+use crate::ascii::render;
 use crate::common::{Angle, Camera, Node, Splat};
 
 mod common;
 
 mod fastfunclib;
+
+mod ascii;
 
 fn main() {
     let mut cam = Camera::new(
@@ -42,27 +46,24 @@ fn main() {
         },
     ];
 
-    let mut percentage: f32;
-    let frames = 48;
-    for index in 0..frames {
-        percentage = index as f32 / frames as f32;
-        cam.node.angle = Angle::new(percentage * TAU, -0.56);
-        cam.node.position = Vec3::new(
-            12.5 + 12.5 * cosf(TAU * percentage + PI),
-            12.5 * sinf(TAU * percentage + PI),
-            8.,
-        );
+    if env::args().len() > 1 {
         cam.render(&splats);
-        cam.write(&index).unwrap();
-        println!("Completed with {index}");
+        render(&cam).unwrap();
+    } else {
+        // Non-realtime ppm rendering
+        let mut percentage: f32;
+        let frames = 48;
+        for index in 0..frames {
+            percentage = index as f32 / frames as f32;
+            cam.node.angle = Angle::new(percentage * TAU, -0.56);
+            cam.node.position = Vec3::new(
+                12.5 + 12.5 * cosf(TAU * percentage + PI),
+                12.5 * sinf(TAU * percentage + PI),
+                8.,
+            );
+            cam.render(&splats);
+            // cam.write(&index).unwrap();
+            println!("Completed with {index}");
+        }
     }
-
-    // println!("{cam:?}");
-    // println!("{:?}", cam.node.angle.to_vec());
-
-    // let r = Ray::from_camera(&cam, [1., 0.]);
-    // println!("{:?}", r);
-
-    // let output_color = r.render_gaussian(splats);
-    // println!("{output_color:?}");
 }
